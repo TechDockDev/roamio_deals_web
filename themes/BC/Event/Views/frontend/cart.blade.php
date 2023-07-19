f05235319df71148d6ff7ad2264e1ede7cfd2adc
SHARRY89 <sharry@techdocklabs.com>
2023-07-18 22:47:55 -0700

    New Changes done

--- themes/BC/Event/Views/frontend/cart.blade.php
+++ themes/BC/Event/Views/frontend/cart.blade.php
@@ -44,7 +44,60 @@
     border: 1px solid #ccc;
     border-radius: 5px;
   }
+  #calendar {
+    max-width: 384px;
+    height: 300px;
+    margin: 0 auto;
+  }
+
+  .fc-toolbar h2 {
+    font-size: 0.999em;
+    margin: 0;
+}
+
+  .fc-dayGridMonth-view .fc-day-grid {
+    width: 100%; /* Adjust the grid cell width */
+  }
+
+  .fc-dayGridMonth-view .fc-day-grid .fc-day {
+    width: 25px; /* Adjust the grid cell size */
+    height: 25px; /* Adjust the grid cell size */
+  }
+  
+  .fc-title{
+
+    background: #FF3500 !Important;
+    border: none !important;
+  }
+   .fc-event, .fc-event-dot {
+      background: #FF3500 !Important;
+    border: none !important;
+      }
+   
+    .fc-dayGrid {
+    border: none;
+  }
+
+    #calendar {
+       border-radius: 10px;
+    }
+
+.fc-day:hover {
+    background-color: #f0f0f0;
+    cursor: pointer;
+    border-radius:12px;
+  }
+
+  .fc-dayGridMonth-view .fc-day-grid .fc-day {
+    width: 25px;
+    height: 25px;
+    border: none;
+}
+
+
     </style>
+
+
 @endpush
 
 @section('content')
@@ -74,9 +127,19 @@ $totalAmount = 0;
                         <span>{{$dd->package_name}}</span>
                        <a href="{{url('deleteCart/'.$dd->id)}}"> <span class="text-end" style="float:right;"><i class="fa fa-trash" aria-hidden="true"></i></span></a>
                     </h5>
-                    <p class="card-text"><i class="fa fa-calendar" aria-hidden="true"></i><span>@if($dd->type == 'hotel')   Staycation @else       
+                    <p class="card-text"><i class="fa fa-calendar" aria-hidden="true"></i><span>@if($dd->type == 'hotel')   Staycation 
+
+                        @else     
+
                             Activity
                     @endif     </span></p>
+                        
+                        <input type="text" name="start_date[]" placeholder="start date"  id="startDate">
+
+                        <input type="text" name="end_date[]" placeholder="end date" id="endDate" >
+
+                        <div id="calendar"></div>
+
                     <p class="card-text"><small><i class="fa fa-users" aria-hidden="true"></i> <span>{{$dd->room_qty}} x Adult</span></small></p>
                     <h5 class="card-text price  pricex{{$dd->id}}">{{$dd->room_price * $dd->room_qty}}<span class="text">AED</span></h5>
                 </div>
@@ -136,6 +199,95 @@ $totalAmount = 0;
   </div>
   
 </div>
+
+
+   
+
+    @push('css')
+    <link rel="stylesheet" href="{{asset('libs/fullcalendar-4.2.0/core/main.css')}}">
+    <link rel="stylesheet" href="{{asset('libs/fullcalendar-4.2.0/daygrid/main.css')}}">
+    <link rel="stylesheet" href="{{asset('libs/daterange/daterangepicker.css')}}">
+
+@endpush
+
+@push('js')
+    <script src="{{asset('libs/daterange/moment.min.js')}}"></script>
+    <script src="{{asset('libs/daterange/daterangepicker.min.js?_ver='.config('app.asset_version'))}}"></script>
+    <script src="{{asset('libs/fullcalendar-4.2.0/core/main.js')}}"></script>
+    <script src="{{asset('libs/fullcalendar-4.2.0/interaction/main.js')}}"></script>
+    <script src="{{asset('libs/fullcalendar-4.2.0/daygrid/main.js')}}"></script>
+
+      <!-- FullCalendar CSS and JavaScript -->
+<link rel="stylesheet" href="{{ asset('libs/fullcalendar-4.2.0/core/main.css') }}">
+<link rel="stylesheet" href="{{ asset('libs/fullcalendar-4.2.0/daygrid/main.css') }}">
+<script src="{{ asset('libs/daterange/moment.min.js') }}"></script>
+<script src="{{ asset('libs/daterange/daterangepicker.min.js?_ver=' . config('app.asset_version')) }}"></script>
+<script src="{{ asset('libs/fullcalendar-4.2.0/core/main.js') }}"></script>
+<script src="{{ asset('libs/fullcalendar-4.2.0/interaction/main.js') }}"></script>
+<script src="{{ asset('libs/fullcalendar-4.2.0/daygrid/main.js') }}"></script>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    // Fetch prices from the backend API
+    fetch('/get_dates_data')
+      .then(response => response.json())
+      .then(data => {
+        var events = data.map(item => ({
+          title: 'AED' + item.price,
+          start: item.date,
+          price: item.price 
+        }));
+         
+        // Initialize FullCalendar with events data
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          plugins: ['interaction', 'dayGrid'],
+          header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          },
+          selectable: true, // Allow date selection
+          select: function(info) {
+            var selectedEvent = events.find(event => event.start === info.startStr);
+            if (selectedEvent) {
+              alert('Selected Date: ' + info.startStr + ' Price: ' + selectedEvent.price);
+            } else {
+              alert('Selected Date: ' + info.startStr + ' Price: Default Price (500)');
+            }
+          },
+          events: events // Use the events array containing the price data
+        });
+
+        // Get the current month's start and end dates
+        // var currentDate = new Date();
+        // var currentYear = currentDate.getFullYear();
+        // var currentMonth = currentDate.getMonth() + 1; // Adding 1 to month since it's 0-indexed
+        // var daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+        // var defaultStartDate = currentYear + '-' + currentMonth + '-01';
+        // var defaultEndDate = currentYear + '-' + currentMonth + '-' + daysInMonth;
+
+        // // Set the values of the hidden form fields with the default date range
+        // document.getElementById('startDateInput').value = defaultStartDate;
+        // document.getElementById('endDateInput').value = defaultEndDate;
+
+        // Submit the form on page load
+        // document.getElementById('dateForm').submit();
+
+        calendar.render();
+      })
+      .catch(error => {
+        console.error('Error fetching price data:', error);
+      });
+  });
+</script>
+
+
+    @endpush
+
+
+
 
 @if(Session::get('successdataadded'))
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
